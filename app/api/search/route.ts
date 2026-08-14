f0f51d2acb947b6baac8f29beb1543454f407ca9
Artemis Li <auroraziling@gmail.com>
2026-08-14 16:59:27 +0800

    fix: search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,13 +1,4 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
-import { createTokenizer as createMandarinTokenizer } from '@orama/tokenizers/mandarin';
 
-export const { GET } = createFromSource(source, {
-  localeMap: {
-    'en': { language: 'english' },
-    'zh': {
-      components: { tokenizer: createMandarinTokenizer() },
-      search: { threshold: 0, tolerance: 0 },
-    },
-  },
-});
+export const { GET } = createFromSource(source);
